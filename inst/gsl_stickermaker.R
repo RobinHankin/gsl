f34795a7e93bd11700eae3a8cda4649f6d5f175d
Robin Hankin <hankin.robin@gmail.com>
2021-08-16 10:20:47 +1200

    hex sticker

---
+++ inst/gsl_stickermaker.R
@@ -0,0 +1,22 @@
+library("hexSticker")
+library("gsl")
+
+
+bmp(file="wrapper.bmp",bg="#7733FF")
+x <- seq(from=-pi,to=pi,len=400)
+plot(x,cos(x),asp=1,type="l",lwd=10,axes=FALSE,xlab="",ylab="")
+
+is.na(x[abs(cos(x))<0.15]) <- TRUE
+
+
+points(x,-cos(x),asp=1,type="l",lwd=10)
+
+segments(+pi,-1,+pi,+1,lwd=10)
+segments(-pi,-1,-pi,+1,lwd=10)
+dev.off()
+
+
+
+sticker("wrapper.bmp", package="gsl", p_size=8, s_x=1, s_y=0.84,
+s_width=0.9,asp=0.85, white_around_sticker=TRUE, h_fill="#7733FF",
+h_color="#000000", filename="gsl.png")
