c989494307a7b27c116d1c60bf099f82d99a6f89
Robin Hankin <rhankin@aut.ac.nz>
2021-09-06 14:19:05 +1200

    font size now correct for windows

--- inst/gsl_stickermaker.R
+++ inst/gsl_stickermaker.R
@@ -18,6 +18,6 @@ dev.off()
 
 
 
-sticker("wrapper.bmp", package="gsl", p_size=8, s_x=1, s_y=0.84,
+sticker("wrapper.bmp", package="gsl", p_size=28, s_x=1, s_y=0.84,
 s_width=0.9,asp=0.85, white_around_sticker=TRUE, h_fill="#7733FF",
 h_color="#000000", filename="gsl.png")
