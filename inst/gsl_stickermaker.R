a2aec1c9a91a91e79ec3115267f36d07b96cc236
Robin Hankin <hankin.robin@gmail.com>
2022-07-21 13:11:27 +1200

    better overlap

--- inst/gsl_stickermaker.R
+++ inst/gsl_stickermaker.R
@@ -5,18 +5,22 @@ library("gsl")
 bmp(file="wrapper.bmp",bg="#7733FF")
 par(mar=c(5,2,4,2)+0.1)
 x <- seq(from=-pi,to=pi,len=400)
-plot(x,cos(x),asp=1,type="l",lwd=10,axes=FALSE,xlab="",ylab="")
 
-is.na(x[abs(cos(x))<0.15]) <- TRUE
+plot(x,-cos(x),asp=1,type="l",lwd=10,axes=FALSE,xlab="",ylab="")
+xx <- x[abs(cos(x))<0.15]
+
+points(xx,cos(xx),asp=1,type="l",lwd=20,col="#7733FF")
+
+points(x,cos(x),asp=1,type="l",lwd=10)
 
 
-points(x,-cos(x),asp=1,type="l",lwd=10)
+
+
+
 
 segments(+pi,-1,+pi,+1,lwd=10)
 segments(-pi,-1,-pi,+1,lwd=10)
 dev.off()
-
-
 
 sticker("wrapper.bmp", package="gsl", p_size=28, s_x=1, s_y=0.84,
 s_width=0.9,asp=0.85, white_around_sticker=TRUE, h_fill="#7733FF",
